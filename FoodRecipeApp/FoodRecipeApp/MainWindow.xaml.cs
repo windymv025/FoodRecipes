e9e1a29604ca23d82676f7bcd5e6b32d269ea84f
ThatPham2000 <thatpham2000@gmail.com>
2020-10-27 16:58:37 +0700

    Home Page

---
+++ FoodRecipeApp/FoodRecipeApp/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FoodRecipeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Set tooltip
+            if(Tg_btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_favorite.Visibility = Visibility.Collapsed;
+                tt_add.Visibility = Visibility.Collapsed;
+                tt_search.Visibility = Visibility.Collapsed;
+                tt_contact.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_favorite.Visibility = Visibility.Visible;
+                tt_add.Visibility = Visibility.Visible;
+                tt_search.Visibility = Visibility.Visible;
+                tt_contact.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+        }
+
+        private void Tg_btn_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+        }
+
+        private void bg_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_btn.IsChecked = false;
+        }
+
+    }
+}
