281b0926cbd8716f6940b7cdbd5a8bc57f548ee3
windymv025 <windymv025@users.noreply.github.com>
2020-11-15 15:24:18 +0700

    Fix project

--- FoodRecipeApp/FoodRecipeApp/MainWindow.xaml.cs
+++ FoodRecipeApp/FoodRecipeApp/MainWindow.xaml.cs
@@ -1,24 +1,7 @@
-﻿using FoodRecipeApp.Models;
-using FoodRecipeApp.ViewModels;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data.Entity;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FoodRecipeApp.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FoodRecipeApp
 {
